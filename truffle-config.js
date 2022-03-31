f7093d5fc678b9d7457410f4b3b999041a55a3a7
Rietveldt <74226561+putamayo@users.noreply.github.com>
2022-03-31 13:27:44 +0200

    rpc  mumbai configured

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,31 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
-
      development: {
-       host: "192.168.2.6",     // Localhost (default: none)
-       port:  7545,            // Standard Ethereum port (default: none)
-       network_id: "5777",       // Any network (default: none)
+       host: "127.0.0.1",     // Localhost (default: none)
+       port:  8545,            // Standard Ethereum port (default: none)
+       network_id: "*",       // Any network (default: none)
      },
+
+     matic: {
+     provider: () => new HDWalletProvider(mnemonic, 'https://rpc-mumbai.maticvigil.com/'),
+
+     network_id: 80001,
+     confirmations: 2,
+     timeoutBlocks: 200,
+     skipDryRun: true
+     },
+
      advanced: {
        websockets: true,
        },
      },
+     mocha: {
+     },
+
   contracts_build_directory: "./src/abis/"
 //  compilers: {
 //    solc: {
