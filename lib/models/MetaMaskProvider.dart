5974340acd9b0f5b3cfc3c6e38297a3004c04547
putamayo <cloud@native.shopping>
2022-04-05 23:57:51 +0200

    update

--- lib/models/MetaMaskProvider.dart
+++ lib/models/MetaMaskProvider.dart
@@ -3,9 +3,4 @@ import 'package:web3dart/browser.dart';
 import 'package:web3dart/web3dart.dart';
 
 class MetaMaskProvider extends ChangeNotifier {
-  static const operatingChain = 4;
-  String currentAddress = "";
-  int currentChain = -1;
-  bool get isEnabled => Ethereum() != null;
-  bool get isInOperatingChain => currentAddress.isNotEmpty;
 }
